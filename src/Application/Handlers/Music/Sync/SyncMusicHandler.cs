ffb9262a344e3615daeefa8a972a93803aa531cc
1111-ssss <gurskievgeni@gmail.com>
2026-03-11 16:14:19 +0300

    feat(music): add syncMusic implementation

--- src/Application/Handlers/Music/Sync/SyncMusicHandler.cs
+++ src/Application/Handlers/Music/Sync/SyncMusicHandler.cs
@@ -1,18 +1,24 @@
 using Application.Interfaces.Common;
+using Application.Interfaces.Music;
 using Domain.Model.Result;
 
 namespace Application.Handlers.Music.Sync;
 
 public class SyncMusicHandler : IHandler<SyncMusicRequest, SyncMusicResponse>
 {
+    private readonly IRadioService _radioService;
     public SyncMusicHandler(
-        
+        IRadioService radioService
     )
     {
-        
+        _radioService = radioService;
     }
-    public Task<Result<SyncMusicResponse>> Handle(SyncMusicRequest request, CancellationToken ct)
+    public async Task<Result<SyncMusicResponse>> Handle(SyncMusicRequest request, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var music = _radioService.GetCurrentlyPlaying();
+        if (music == null)
+            return Result.Failed(ErrorCode.NotFound, "Нет проигрываемой музыки");
+        
+        return Result<SyncMusicResponse>.Success(music);
     }
 }
