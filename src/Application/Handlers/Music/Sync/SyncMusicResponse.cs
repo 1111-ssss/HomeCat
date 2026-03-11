369135fb97e88a0a586624f6f520b3c208f8ff22
1111-ssss <gurskievgeni@gmail.com>
2026-03-11 15:52:19 +0300

    refact(syncMusicResponse): change fields type

--- src/Application/Handlers/Music/Sync/SyncMusicResponse.cs
+++ src/Application/Handlers/Music/Sync/SyncMusicResponse.cs
@@ -3,9 +3,6 @@ using Application.Handlers.Music.Common;
 namespace Application.Handlers.Music.Sync;
 
 public record SyncMusicResponse(
-    string TrackId,
-    string Artist,
-    string Album,
     string Title,
     int Duration,
     int Position,
