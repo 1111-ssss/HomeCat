f05a1ea0c6c01700cb10c9efb7ea3cf76c917059
1111-ssss <gurskievgeni@gmail.com>
2026-03-11 15:58:07 +0300

    feat(api): added endpoints to di

--- src/Web/Extensions/Bootstrapper/MapEndpointsExtensions.cs
+++ src/Web/Extensions/Bootstrapper/MapEndpointsExtensions.cs
@@ -7,6 +7,8 @@ public static class MapEndpointsExtensions
     public static WebApplication MapEndpoints(this WebApplication app)
     {
         app.MapAuthEndpoints();
+        app.MapMusicEndpoints();
+        app.MapDownloadEndpoints();
 
         return app;
     }
