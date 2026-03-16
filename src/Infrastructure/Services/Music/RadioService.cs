684ef5177f5e9f0141ae4d09c4344fd038ea8673
1111-ssss <gurskievgeni@gmail.com>
2026-03-16 06:14:01 +0300

    refact(radioService): change lastresponsed to startedat

--- src/Infrastructure/Services/Music/RadioService.cs
+++ src/Infrastructure/Services/Music/RadioService.cs
@@ -6,31 +6,32 @@ namespace Infrastructure.Services.Music;
 public class RadioService : IRadioService
 {
     private SyncMusicResponse? _currentlyPlaying;
-    private DateTime _lastResponsed = DateTime.UtcNow;
+    private DateTime _startedAt = DateTime.UtcNow;
+    
     public RadioService()
     {
-        
+
     }
+    
     public void Play(SyncMusicResponse file)
     {
         _currentlyPlaying = file;
+        _startedAt = DateTime.UtcNow;
     }
+    
     public SyncMusicResponse? GetCurrentlyPlaying()
     {
         if (_currentlyPlaying == null)
             return null;
 
-        var currentPosition = (DateTime.UtcNow - _lastResponsed).TotalSeconds % _currentlyPlaying.Duration;
+        var currentPosition = (DateTime.UtcNow - _startedAt).TotalMilliseconds % _currentlyPlaying.Duration;
 
-        var updatedMusic = new SyncMusicResponse(
+        return new SyncMusicResponse(
             Title: _currentlyPlaying.Title,
             Duration: _currentlyPlaying.Duration,
             Position: currentPosition,
             ServerTimestamp: DateTime.UtcNow,
             File: _currentlyPlaying.File
         );
-        _currentlyPlaying = updatedMusic;
-
-        return updatedMusic;
     }
 }
